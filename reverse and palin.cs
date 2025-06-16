e13b928f7511fc44d1558d4aa8ac15bd173fb9b8
Payal shirose <159715853+Payal109@users.noreply.github.com>
2025-06-16 23:51:32 +0530

    Add files via upload

---
+++ reverse and palin.cs
@@ -0,0 +1,28 @@
+﻿namespace Reverse_a_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter to REVERSE A NUM");
+            int num = Convert.ToInt32 (Console.ReadLine());
+            int original_num = num;
+            int reverse=0;
+            int digit;
+            while (num > 0)
+            {
+                digit = num % 10;
+                reverse = reverse * 10 + digit; ;
+                num /= 10;
+
+            }
+        Console.WriteLine($"The reverse of {original_num} is {reverse}");
+            if (reverse == original_num) 
+            {
+                Console.WriteLine(" The number is Palindrome");
+            }
+            else 
+            { Console.WriteLine("Number is not a Palindrome"); }
+        }
+    }
+}
