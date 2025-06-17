b42cae450d1d23ffe59a0c26ee37a0e3dac79e88
Payal shirose <159715853+Payal109@users.noreply.github.com>
2025-06-17 21:08:25 +0530

    Add files via upload

---
+++ count_words.cs
@@ -0,0 +1,39 @@
+﻿namespace count_words
+{
+    internal class countwords
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---count words---");
+            Console.WriteLine("enter a setence");
+            String sentence = Console.ReadLine();
+            int WordCount = 0;
+            bool Inword = false;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                char c = sentence[i];
+
+                if (c != ' ' && c != '\t' && c != '\n')
+
+                {
+                    if (!Inword)
+                    {
+                        WordCount++;
+                        Inword = true;
+                    }
+                }
+
+                else
+                {
+                    Inword = false;
+                }
+            }
+        Console.WriteLine(" the count of words is : " + WordCount);
+
+
+
+            
+        }
+    }
+}
