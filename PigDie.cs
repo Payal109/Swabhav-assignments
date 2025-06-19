2b2fe0c3446828da31cc3a29499aee6d696f9225
Payal shirose <159715853+Payal109@users.noreply.github.com>
2025-06-19 20:45:43 +0530

    Add files via upload

---
+++ PigDie.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace PigDieGame
+{
+    internal class Pig_Die
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--WELCOME TO PIG DIE---");
+            int numberOfTurns = 0;
+            int totalScore = 0;
+            int die;
+            Random ran = new Random();
+            do
+            {
+                int turnScore = 0;
+                numberOfTurns++;
+                Console.WriteLine(" TURN -- " + numberOfTurns);
+                string choice = "";
+
+                
+                    Console.WriteLine("roll or hold? (r/h)");
+                    choice = Console.ReadLine();
+                while (choice == "r")
+                {
+                    die = ran.Next(1, 7);
+                    Console.WriteLine("DIE : " + die);
+
+                    if (die == 1)
+                    {
+                        Console.WriteLine("TURN OVER. NO SCORE");
+                        turnScore = 0;
+                        break;
+
+
+                    }
+                    else
+                    {
+                        turnScore = turnScore + die;
+                        Console.WriteLine($"Turn score : {turnScore}");
+                        Console.WriteLine("roll or hold? (r/h)");
+                        choice = Console.ReadLine();
+
+
+                    }
+                }
+                    
+
+                
+            if (choice == "h")
+             {
+             totalScore = totalScore + turnScore;
+             Console.WriteLine($"Score for turn : {turnScore}");
+              Console.WriteLine($"Total Score : {totalScore}");
+
+             }
+            } while (totalScore < 20);
+
+            Console.WriteLine($"you finished in {numberOfTurns}");
+        }
+    }
+}
