8620d988a3eda042f4317860642acd58bb64805f
Payal shirose <159715853+Payal109@users.noreply.github.com>
2025-06-21 17:56:35 +0530

    Add files via upload

---
+++ VotingAgeCalc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class VotingAgeCalc
+{
+    public int LegalAge= 18;// avoiding magic number
+    public string Name {  get; set; }
+	public int Age { get; set; }
+
+	public VotingAgeCalc(string name, int age)
+		{ this.Age = age;
+		  this.Name = name;
+	}
+
+	public void Eligibilitycheck()
+	{
+		if (this.Age >= LegalAge)
+
+		{
+			Console.WriteLine($" {this.Name}, You are Eligibile to vote");
+		}
+
+		else
+		{
+			Console.WriteLine($"{this.Name}, you are not eligible to vote");
+		}
+	}
+
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("enter your name ");
+		string first = Console.ReadLine();
+		VotingAgeCalc obj1 = new VotingAgeCalc(first, 23);
+		obj1.Eligibilitycheck();
+
+        Console.WriteLine("enter your name ");
+        string second = Console.ReadLine();
+        VotingAgeCalc obj2 = new VotingAgeCalc(second, 14);
+        obj2.Eligibilitycheck();
+
+
+
+    }
+
+	
+}
+
---
+++ this_keyword.cs
@@ -0,0 +1,33 @@
+﻿namespace thisKeyword
+{
+     class This_keyword
+    {
+        //these are fields i.e varibales declared inside class but outside a method
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        //
+        public This_keyword(string name, double price)
+        {
+            this.Name = name; //this.Name refers to field and name is the paramter passed while calling
+            this.Price = price;
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"Car Brand : {this.Name}");
+            Console.WriteLine($"Price : {this.Price}");
+
+        }
+        static void Main(string[] args)
+        {
+            This_keyword car1 = new This_keyword("Toyota", 45000);
+            car1.display();
+            Console.WriteLine("enter 2nd car name and price");
+            string input = Console.ReadLine();
+            double input2 = Convert.ToInt32(Console.ReadLine());
+            This_keyword car2 = new This_keyword(input, input2);
+            car2.display();
+        }
+    }
+}
