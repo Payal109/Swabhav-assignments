01aa9ebd4b7ae78ede4f0a3e2cc5ffc88c4a0a3b
Payal shirose <159715853+Payal109@users.noreply.github.com>
2025-06-21 12:53:08 +0530

    Add files via upload

---
+++ stringapp.cs
@@ -0,0 +1,15 @@
+﻿namespace StringManipulation
+{
+    internal class StringTask
+    {
+        static void Main(string[] args)
+        {
+            string input = args[0];
+            string[] parts = input.Split(',');
+
+            Console.WriteLine("Developer name is : " + parts[0]);
+            Console.WriteLine("Company name is : " + parts[1]);
+            Console.WriteLine($"Location is : {parts[2]}"); 
+        }
+    }
+}
