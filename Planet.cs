faae04b1e4e4f8633c6ce0604874e48372e382fc
Payal shirose <159715853+Payal109@users.noreply.github.com>
2025-06-22 12:22:32 +0530

    Add files via upload

---
+++ Planet.cs
@@ -0,0 +1,45 @@
+﻿namespace PlanetEnum
+{
+    internal class Planet
+    {
+
+        public enum Planett
+        {
+            MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE
+        }
+
+        public string Name { get; set; }
+        public double GravityFactor { get; set; }
+
+        public Planet(string name, double gravityFactor)
+        {
+            this.Name = name;
+            this.GravityFactor = gravityFactor;
+        }
+
+       
+        static void Main(string[] args)
+        {
+            List<Planet> planets = new List<Planet>()
+        {
+            new Planet("MERCURY" , 0.38),
+            new Planet("VENUS" , 0.91),
+            new Planet("EARTH" , 1.0),
+            new Planet("MARS" , 0.38),
+            new Planet("SATURN" , 1.06),
+            new Planet("URANUS" , 0.92),
+            new Planet("NEPTUNE" , 1.19),
+        };
+            Console.WriteLine("enter weight on earth");
+
+
+            double weightOnEarth = Convert.ToDouble(Console.ReadLine());
+
+            for (int i = 0; i < planets.Count; i++)
+            {
+                double newWeight = weightOnEarth * planets[i].GravityFactor;
+                Console.WriteLine($"Weight on {planets[i].Name} is {newWeight:F2}");
+            }
+        }
+    }
+}
---
+++ Week.cs
@@ -0,0 +1,25 @@
+﻿namespace WeekEnum
+{
+    internal class Week
+    {
+        public enum Day
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday }
+        
+        static void Main(string[] args)
+        {
+            Day today = Day.Monday;
+            Console.WriteLine(today);
+            Console.WriteLine("enter a number (1-7)");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Day selectedday = (Day)input;
+            Console.WriteLine($"Day is : {selectedday}");
+        }
+    }
+}
